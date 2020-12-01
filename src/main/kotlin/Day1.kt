ffde88b530f9be00da0b07ebbe84924bd3098e71
Alex Parlett <alexparlett@gmail.com>
2020-12-01 21:53:19 +0000

    [Day1] Switch to generating all combinations and then finding one which matches

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,51 +1,10 @@
-fun twoSum(nums: List<Int>, target: Int): IntArray {
-    var found = nums.find { num -> nums.contains(target - num) }
-    if (found != null)
-        return intArrayOf(found, target - found)
-    throw IllegalArgumentException("No twoSum solution")
-}
-
-fun threeSum(nums: List<Int>, target: Int): IntArray {
-    for (i in nums.indices) {
-        for (j in i + 1 until nums.size) {
-            for (k in j + 1 until nums.size) {
-                if (nums[i] + nums[j] + nums[k] == target) {
-                    return intArrayOf(nums[i], nums[j], nums[k])
-                }
-            }
-        }
-    }
-    throw IllegalArgumentException("No threeSum solution")
-}
-
-
-fun part1(lines: List<Int>, target: Int): Int? {
-    var found = twoSum(lines, target)
-
-    if (found != null) {
-        return found.reduce { a,b -> a * b}
-    }
-
-    return null
-}
-
-fun part2(lines: List<Int>, target: Int): Int? {
-    var found = threeSum(lines, target)
-
-    if (found != null) {
-        return found.reduce { a,b -> a * b}
-    }
-
-    return null
-}
-
 fun main(args: Array<String>) {
-    var lines = readLines("src/main/resources/Day1.txt")
+    val lines = readLines("src/main/resources/Day1.txt")
         .map { line -> line.toInt() }
 
-    var part1Result = part1(lines, 2020)
-    println(part1Result)
+    val cominations2 = CombinationGenerator(lines, 2)
+    println(cominations2.find { item -> item.sum() == 2020}?.reduce{ a,b -> a * b})
 
-    var part2Result = part2(lines, 2020)
-    println(part2Result)
+    val cominations3 = CombinationGenerator(lines, 3)
+    println(cominations3.find { item -> item.sum() == 2020}?.reduce{ a,b -> a * b})
 }
--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -1,11 +1,49 @@
 import java.io.File
 
-/**
- * Copyright (c) 2015-2020 Homonoia Studios.
- *
- * @author alexparlett
- * @since 01/12/2020
- */
-
 fun readLines(path: String): List<String>
         = File(path).readLines()
+
+class CombinationGenerator<T>(private val items: List<T>, choose: Int = 1) : Iterator<List<T>>, Iterable<List<T>> {
+    private val indices = Array(choose) { it }
+    private var first = true
+
+    init {
+        if (items.isEmpty() || choose > items.size || choose < 1)
+            error("list must have more than 'choose' items and 'choose' min is 1")
+    }
+
+    override fun hasNext(): Boolean = indices.filterIndexed { index, it ->
+        when (index) {
+            indices.lastIndex -> items.lastIndex > it
+            else -> indices[index + 1] - 1 > it
+        }
+    }.any()
+
+    override fun next(): List<T> {
+        if (!hasNext()) error("Unexpected end to combinations")
+        if (!first) {
+            incrementAndCarry()
+        } else
+            first = false
+        return List(indices.size) { items[indices[it]] }
+    }
+
+    private fun incrementAndCarry() {
+        var carry: Boolean
+        var place = indices.lastIndex
+        do {
+            carry = if ((place == indices.lastIndex && indices[place] < items.lastIndex)
+                    || (place != indices.lastIndex && indices[place] < indices[place + 1] - 1)) {
+                indices[place]++
+                (place + 1..indices.lastIndex).forEachIndexed { index, i ->
+                    indices[i] = indices[place] + index + 1
+                }
+                false
+            } else
+                true
+            place--
+        } while (carry && place > -1)
+    }
+
+    override fun iterator(): Iterator<List<T>> = this
+}
